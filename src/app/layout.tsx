993775b5f4ee6965c6409fa1d5fa3dd69a6d5179
moebqr <71250489+moebqr@users.noreply.github.com>
2024-09-23 21:41:12 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,7 @@ import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
-
-export const fontSans = GeistSans;
+import { ThemeProvider } from "../components/theme-provider"
 
 export const metadata: Metadata = {
   title: "Introduction to LLMs",
@@ -11,25 +10,19 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-import { ThemeProvider } from "../components/theme-provider"
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <html lang="en" suppressHydrationWarning>
-      <head />
-        <body>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
-        </body>
-      </html>
-    </>
-
+    <html lang="en" suppressHydrationWarning className={GeistSans.className}>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
   )
 }
