f7fbf416c96f32802acb5b2a9b5a9aa1e52a72b4
moebqr <71250489+moebqr@users.noreply.github.com>
2024-09-23 21:49:16 +0300

    Update rainbow-button.tsx

--- src/components/magicui/rainbow-button.tsx
+++ src/components/magicui/rainbow-button.tsx
@@ -2,8 +2,11 @@ import React from "react";
 
 import { cn } from "~/lib/utils";
 
-interface RainbowButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+export interface RainbowButtonProps {
+  // Add properties here, for example:
+  onClick?: () => void;
+  children: React.ReactNode;
+}
 
 export function RainbowButton({ children, ...props }: RainbowButtonProps) {
   return (
